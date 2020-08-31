5ab8e134a0c83776fe92b94d8fb43209bb6cc088
duyanhnguyenxuan <duyanh1202tm@gmail.com>
2020-08-31 20:20:39 +0700

    update dependencies

--- routes/app.route.js
+++ routes/app.route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-var moment = require('moment');
 //--------------------Dummy Database-----------------------------------
 // users = [];
 
